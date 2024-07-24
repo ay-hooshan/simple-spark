2501a78871f1786c6b55a48727c323a11b00b8ec
abbasyazdanmehr <kjkj55047@gmail.com>
2024-07-24 13:08:51 +0330

    feat: convert json incoming data from kafka to dataframe

--- main.py
+++ main.py
@@ -1,41 +1,54 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col, expr, base64, udf, upper
+from pyspark.sql.functions import from_json, col, expr, upper
 from pyspark.sql.types import StructType, StringType, StructField, IntegerType
 import pyspark.pandas as ps
 
-spark = SparkSession \
-          .builder \
-          .appName("APP") \
-          .getOrCreate()
+# Create Spark session
+spark = SparkSession.builder.appName("APP").getOrCreate()
           
-# this line is 
+# Set checkpoint location
 spark.conf.set("spark.sql.streaming.checkpointLocation", "./spark-storagee")
           
-# input from kafka topics
-df = spark\
-      .readStream \
-      .format("kafka") \
-      .option("kafka.bootstrap.servers", "localhost:9094") \
-      .option("subscribe", "test1") \
-      .option("startingOffsets", "earliest") \
-      .load()
+# Define schema for the JSON data (example schema)
+json_schema = StructType([
+    StructField("title", StringType(), True),
+    StructField("description", StringType(), True)
+])
 
-# see the schema
-print(df.dtypes) # [('key', 'binary'), ('value', 'binary'), ('topic', 'string'), ('partition', 'int'), ('offset', 'bigint'), ('timestamp', 'timestamp'), ('timestampType', 'int')]
+# Read from Kafka
+df = spark.readStream.format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9094") \
+    .option("subscribe", "test1") \
+    .option("startingOffsets", "earliest") \
+    .load()
 
-df = df.withColumn("value", upper(col("value")))  # Example string transformation
+# Print schema
+print(df.dtypes)
 
-# output to console
-query = df.selectExpr("CAST(value AS STRING)") \
-    .writeStream \
+# Transform the data
+# 1. Decode the value from binary to string
+df = df.withColumn("value", col("value").cast("string"))
+
+# 2. Parse the JSON content
+df = df.withColumn("jsonData", from_json(col("value"), json_schema))
+
+# 3. Select the fields from the JSON data and add a new field by transforming the string
+df = df.select(col("key").cast("string"), col("jsonData.*"))
+df = df.withColumn("transformedtitle", upper(col("title")))  # Example string transformation
+
+# Output to console
+query_console = df.writeStream \
     .format("console") \
     .start()
-    
-# output to another kakfa topic    
-query = df.writeStream.outputMode("append").format("kafka") \
-        .option("kafka.bootstrap.servers", "localhost:9094") \
-        .option("topic", "transformed") \
-        .start()
-    
 
-query.awaitTermination()
+# Output to another Kafka topic
+query_kafka = df.selectExpr("CAST(key AS STRING)", "to_json(struct(*)) AS value") \
+    .writeStream \
+    .outputMode("append") \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9094") \
+    .option("topic", "transformed") \
+    .start()
+    
+query_console.awaitTermination()
+query_kafka.awaitTermination()
--- test.py
+++ test.py
@@ -1,2 +1,49 @@
+from confluent_kafka import Producer
+import json
 
+def delivery_report(err, msg):
+    if err is not None:
+        print(f"Message delivery failed: {err}")
+    else:
+        print(f"Message delivered to {msg.topic()} [{msg.partition()}]")
 
+def json_serializer(data):
+    return json.dumps(data).encode("utf-8")
+
+conf = {
+    "bootstrap.servers": "localhost:9094",
+}
+
+producer = Producer(conf)
+
+test_data = [
+    {
+        "x": "1", 
+        "y": "2",
+        "z": "3"
+    }, 
+    {
+        "x": "7", 
+        "y": "2",
+        "z": "4"
+    }, 
+    {
+        "x": "9", 
+        "y": "2",
+        "z": "1"
+    }, 
+    {
+        "x": "3", 
+        "y": "7",
+        "z": "8"
+    }, 
+    {
+        "x": "5", 
+        "y": "8",
+        "z": "0"
+    },
+]
+
+for i in range(len(test_data)):
+    producer.produce("testnum", json_serializer(test_data[i]), callback=delivery_report)
+    producer.flush()
