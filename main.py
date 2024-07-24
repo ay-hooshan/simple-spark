93e97ba3a6332fde742a5a5036b4c5a827f0824e
abbasyazdanmehr <kjkj55047@gmail.com>
2024-07-24 12:08:00 +0330

    feat: transform value column to uppercase string

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
-from pyspark.sql.types import StructType, StringType
+from pyspark.sql.functions import from_json, col, expr, base64, udf, upper
+from pyspark.sql.types import StructType, StringType, StructField, IntegerType
+import pyspark.pandas as ps
 
 spark = SparkSession \
           .builder \
@@ -18,14 +19,14 @@ df = spark\
       .option("subscribe", "test1") \
       .option("startingOffsets", "earliest") \
       .load()
-      
 
 # see the schema
 print(df.dtypes) # [('key', 'binary'), ('value', 'binary'), ('topic', 'string'), ('partition', 'int'), ('offset', 'bigint'), ('timestamp', 'timestamp'), ('timestampType', 'int')]
 
+df = df.withColumn("value", upper(col("value")))  # Example string transformation
 
 # output to console
-query = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "CAST(value AS STRING)") \
+query = df.selectExpr("CAST(value AS STRING)") \
     .writeStream \
     .format("console") \
     .start()
